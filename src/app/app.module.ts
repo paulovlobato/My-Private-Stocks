43f1aaee1350c9ae06a0747b0104b897a8305ce4
paulo.victor@acception.com.br <pvlobato@gmail.com>
2018-08-23 16:14:59 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Plugins
+import { HttpClientModule } from '@angular/common/http'
+
+// Services
+import { StockService } from './services/stock.service'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ 
+    StockService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
